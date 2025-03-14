e112b24e072d0101b3842f4072d6d8c7389d9361
Your Name <you@example.com>
2025-03-14 14:05:17 +1030

    minor fixed few files3

--- Referee.cpp
+++ Referee.cpp
@@ -4,22 +4,28 @@ Player* Referee::refGame(Player* player1, Player* player2) {
     Move* move1 = player1->makeMove();
     Move* move2 = player2->makeMove();
 
-    // Check for a tie case
+    
     if (move1->getName() == move2->getName()) {  
         delete move1;
         delete move2;
+
+        
+        if (player1->getName() == "Computer" && player2->getName() == "Computer") {
+            return player2;  
+        }
+
         return player1;  
     }
 
     Player* winner = nullptr;
     if (move1->beats(move2)) {
         winner = player1;
-    } else if (move2->beats(move1)) {
+    } else {
         winner = player2;
     }
 
     delete move1;
     delete move2;
 
-    return winner;
+    return winner;  
 }
