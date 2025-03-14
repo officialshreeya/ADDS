0e121fd20102cb907e3311d198b461f59a64dadf
Your Name <you@example.com>
2025-03-14 13:52:49 +1030

    fixed few files2

--- Computer.cpp
+++ Computer.cpp
@@ -1,16 +1,8 @@
 #include "Computer.h"
 #include "Rock.h"
-#include "Paper.h"
-#include "Scissors.h"
-#include "Monkey.h"
-#include "Robot.h"
-#include "Pirate.h"
-#include "Ninja.h"
-#include "Zombie.h"
-#include <cstdlib>
 
 Computer::Computer() {
-    fixedMove = nullptr;  
+    fixedMove = new Rock();  
 }
 
 Computer::Computer(Move* move) {
@@ -18,25 +10,9 @@ Computer::Computer(Move* move) {
 }
 
 Move* Computer::makeMove() {
-    if (fixedMove) {
-        return fixedMove;  
-    }
-
-    int choice = rand() % 8;
-    switch (choice) {
-        case 0: return new Rock();
-        case 1: return new Paper();
-        case 2: return new Scissors();
-        case 3: return new Monkey();
-        case 4: return new Robot();
-        case 5: return new Pirate();
-        case 6: return new Ninja();
-        case 7: return new Zombie();
-        default: return new Rock();
-    }
+    return new Rock();  
 }
 
 std::string Computer::getName() {
     return "Computer";
 }
-
