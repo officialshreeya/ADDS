22ee8931a6a3302e4976dde41dce64e9825c4095
officialshreeya <shreeyalve@gmail.com>
2025-03-21 23:11:24 +1030

    please work

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 #include "Truckloads.h"
 int main() {
     Tloads t;
-    std::cout << "Example 0: " << t.numTrucks(14, 3) << std::endl;  // Output: 6
+    std::cout << "Example 0: " << t.numTrucks(14, 3) << std::endl; 
     return 0;
 }
