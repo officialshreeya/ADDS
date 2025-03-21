9feae2da477a8bf0ee8178b78f3fbf27a0db7c2b
officialshreeya <shreeyalve@gmail.com>
2025-03-22 00:46:03 +1030

    fix minor errors

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 int main() {
     //4Truckloads
     Truckloads T;
-    int n1 = 14, n2 = 3;
+    int n1 = 17, n2 = 4;
     std::cout << "Number of trucks: " << T.numTrucks(n1, n2) << std::endl;
     //4Reverser
     Reverser myReverser;
-    int rev_i = myReverser.reverseDigit(12345);
+    int rev_i = myReverser.reverseDigit(12234);
     std::cout << "Reversed int: " << rev_i << std::endl;
-    std::string reversedStr = myReverser.reverseString("hello");
+    std::string reversedStr = myReverser.reverseString("Worlddd");
     std::cout << "Reversed string: " << reversedStr << std::endl;
     return 0;
 }
